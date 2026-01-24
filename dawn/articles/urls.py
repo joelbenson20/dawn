d10bfe91280996a8eb1629a1abce2f2f35c14527
joelbenson20 <joelbenson20@gmail.com>
2026-01-23 19:46:57 -0600

    more template formatting, basic styling

--- dawn/articles/urls.py
+++ dawn/articles/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.articles, name='articles'),
     path('<int:year>/<int:month>/<int:day>/<str:slug>', views.article, name='article'),
 ]
--- dawn/home/views.py
+++ dawn/home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from articles.models import Article
 
 def index(request):
 
-    context = {}
+    articles = Article.objects.filter(published=True).order_by('-publication_date')
+
+    context = {'articles': articles}
 
     return render(request, 'index.html', context=context)
