b06bdce00d395c004150a7ebaeacb6ee944ce56b
joelbenson20 <joelbenson20@gmail.com>
2026-01-25 21:02:15 -0600

    introduced dotenv file and changed settings for DEBUG and SECRET_KEY

--- dawn/dawn/settings.py
+++ dawn/dawn/settings.py
@@ -9,21 +9,24 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j^zw3^4f5_h*b1-l2a+5vykulb%(&vg69tv_oh&fj!5!ci0*g9'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-j^zw3^4f5_h*b1-l2a+5vykulb%(&vg69tv_oh&fj!5!ci0*g9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
