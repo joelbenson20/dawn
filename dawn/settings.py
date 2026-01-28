e68d24db3d69b7e735447aa953fdeaccc07213e2
joelbenson20 <joelbenson20@gmail.com>
2026-01-27 23:16:08 -0600

    allowed hosts and csrf

--- dawn/settings.py
+++ dawn/settings.py
@@ -31,6 +31,8 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['dawn-production-dade.up.railway.app', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://dawn-production-dade.up.railway.app']
+
 
 # Application definition
 
