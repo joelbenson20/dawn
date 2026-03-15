7be960893fce6583b4d2b2e73f60fdf9ba7c9c62
joelbenson20 <joelbenson20@gmail.com>
2026-03-15 01:19:48 -0500

    refactoring

--- articles/utils.py
+++ articles/utils.py
@@ -1,7 +1,7 @@
 content_image_html = """
     <figure class="w-75 mx-auto my-5">
         <img class="img-fluid content-image" src="%s">
-        <p class="content-image-caption  pt-2">%s</p>
+        <small class="content-image-caption  pt-2">%s</small>
     </figure>"""
 
 def embed_image(match_string, article):
