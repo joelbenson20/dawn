7ac88235feb870fc8e65fa99e917864d4b2f00b8
joelbenson20 <joelbenson20@gmail.com>
2026-03-17 05:02:11 -0500

    image caption with br

--- hyper/templates/hyperrender_modules.py
+++ hyper/templates/hyperrender_modules.py
@@ -1,6 +1,6 @@
 image_module = """
     <figure class="w-75 mx-auto my-5">
-        <img class="img-fluid content-image" src="%s">
+        <img class="img-fluid content-image" src="%s"><br>
         <small class="content-image-caption  pt-2">%s</small>
     </figure>
 """
